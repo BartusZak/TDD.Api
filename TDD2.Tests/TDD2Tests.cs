3d2ebac06aa072fc25fa608050d3ce2dc9cee708
BartusZak <b.ploszynski@gmail.com>
2018-03-12 09:28:43 +0100

    Kopiuje GETHASHS W 2 MIEJSCACH.

--- TDD2.Tests/TDD2Tests.cs
+++ TDD2.Tests/TDD2Tests.cs
@@ -39,6 +39,17 @@ namespace TDD.Tests
 
             Assert.Equal(user.Email, email);
         }
+        private string GetHash(string text)
+        {
+            // SHA512 is disposable by inheritance.  
+            using (var sha256 = SHA256.Create())
+            {
+                // Send a sample text to hash.  
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                // Get the hashed string.  
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
 
         public class User : Entity
         {
