47f314722010442f55a5795c3d002e39c0fba0c5
BartusZak <b.ploszynski@gmail.com>
2018-03-19 10:10:17 +0100

    Dodanie Configuration Managera.

---
+++ TDD/ConfigurationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using TDD.Interface;
+
+namespace TDD
+{
+    public class ConfigurationManager:IConfigurationManager
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetValue(string key)
+        {
+            return _configuration[key];
+        }
+
+
+    }
+}
