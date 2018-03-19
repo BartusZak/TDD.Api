423b78296adb97e5af65a705764ef51c051890d7
BartusZak <b.ploszynski@gmail.com>
2018-03-19 09:12:25 +0100

    Dodanie Startup

--- TDD.WebApi/Program.cs
+++ TDD.WebApi/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace TDD.WebApi
 {
---
+++ TDD.WebApi/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TDD.WebApi
+{
+    class Startup
+    {
+
+        private IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddMvc();
+        }
+    }
+}
